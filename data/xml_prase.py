67561baa70db139f3864a794b3ec565818505840
HenryLiu <583448542@qq.com>
2018-04-02 13:41:55 +0800

    prase xml with category

--- data/xml_prase.py
+++ data/xml_prase.py
@@ -12,7 +12,8 @@ class MovieHandler( xml.sax.ContentHandler ):
       self.CurrentData = ""
       self.subject = ""
       self.content = ""
-      self.ques = ""
+      self.maincat = ""
+      self.ques = {}
  
     # 元素开始事件处理 
     def startElement(self, tag, attributes):
@@ -22,13 +23,22 @@ class MovieHandler( xml.sax.ContentHandler ):
    # 元素结束事件处理
     def endElement(self, tag):
         self.CurrentData = ""
-        if tag == "content":
-            self.ques = self.ques + self.subject + " " + self.content + "\n"
+        if tag == "maincat":
+            question = self.subject + " " + self.content
+            if self.ques.has_key(self.maincat):
+                self.ques[self.maincat].append(question)
+            else:
+                self.ques[self.maincat] = [question]
+            self.maincat = ""
             self.subject = ""
             self.content = ""
         if tag == "ystfeed":
             with open(output_file, 'w') as fout:
-                fout.write(self.ques)
+                for maincat, ques in self.ques.items():
+                    print("maincat: ", maincat, "total questions: ", len(ques))
+                    fout.write("<maincat> " + maincat + "\n")
+                    for q in ques:
+                        fout.write(self.ques + "\n")
             #print(self.ques)
     
     # 内容事件处理
@@ -37,6 +47,8 @@ class MovieHandler( xml.sax.ContentHandler ):
             self.subject += content.strip()
         elif self.CurrentData == "content":
             self.content += content.strip()
+        elif self.CurrentData == "maincat":
+            self.maincat += content.strip()
             
     
 if (  __name__ == "__main__"):
