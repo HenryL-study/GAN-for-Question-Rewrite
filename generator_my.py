6b0795c08aac54c5db99ddb3eddd11bba75a49ca
HenryLiu <583448542@qq.com>
2018-04-07 16:06:23 +0800

    fix length bug

--- generator_my.py
+++ generator_my.py
@@ -87,13 +87,15 @@ class Generator(object):
         #print("id", predicting_decoder_output.sample_id)
         #print("rnn", predicting_decoder_output.rnn_output)
         self.g_predictions = predicting_decoder_output.rnn_output
-        #pad
-        # len_to_fill = self.max_sequence_length - tf.shape(self.g_predictions)[1]
-        # #print("len_to_fill: ", len_to_fill)
-        # paddings = [[0,0],[0,len_to_fill],[0,0]]
-        # self.g_predictions = tf.pad(self.g_predictions, paddings)
         self.g_samples = predicting_decoder_output.sample_id
         self.g_rollout = rollout_decoder_output.sample_id
+        #pad
+        len_to_fill = self.max_sequence_length - tf.shape(self.g_predictions)[1]
+        #print("len_to_fill: ", len_to_fill)
+        paddings = [[0,0],[0,len_to_fill],[0,0]]
+        self.g_predictions = tf.pad(self.g_predictions, paddings)
+        self.g_samples = tf.pad(self.g_samples, [[0,0],[0,len_to_fill]])
+        self.g_rollout = tf.pad(self.g_rollout, [[0,0],[0,len_to_fill]])
         self.g_loss = -tf.reduce_sum(
             tf.reduce_sum(
                 tf.one_hot(tf.to_int32(tf.reshape(self.x, [-1])), self.num_emb, 1.0, 0.0) * tf.log(
